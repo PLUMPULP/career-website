08a66bb796a58beb56dbb7cddc430fe4b8a5c6ed
Akhil S <127322758+PLUMPULP@users.noreply.github.com>
2023-03-09 18:05:06 +0000

    made changes

--- app.py
+++ app.py
@@ -35,9 +35,10 @@ def hello_world():
   return render_template("home.html", jobs=JOBS)
 
 
-@app.route("/api/jobs"):
-def list_jobs();
-jsonify(JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
